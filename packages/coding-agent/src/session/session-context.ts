Sharp336/grimoire-agent-engine

address codex round 60: sibling rollback markers, pid start-identity in prune marker
- rollback ownership markers (userTurn / promptPrelude) move out of the
  extension-owned details payload onto CustomMessage/CustomMessageEntry
  as sibling fields: CustomMessage<T = unknown> allows primitive and
  array details, so a details-nested marker either coerced the payload
  into a character-keyed record or (after the guarded merge) silently
  dropped recognition for those prompts, leaving /undo skipping real
  turns and prompt-owned preludes on the branch. stampCustomMessageMarker
  sets the sibling flag and never touches details; #persistMessageEnd
  forwards it to appendCustomMessageEntry's new ownership param, and
  session-context replay restores it onto the reconstructed message
- #isUserTurnEntry / #isPromptPreludeEntry read the sibling markers
  (customType allowlists remain the legacy-journal fallback);
  XdevMountNoticeDetails drops its promptPrelude field
- regression: an extension prompt with a primitive details payload is
  recognized and undoable (fails under details-based recognition), and
  stamping preserves details of every shape while setting the marker
- prune-marker stale detection binds to the process start identity:
  markUndoTailPruneInProgress records the /proc starttime beside the pid,
  and isProcessInstanceAlive requires the occupying process to be the
  same launch — a recycled pid no longer masquerades as the live
  pruner, so opens no longer block on the 30s wait for a marker whose
  writer is gone. Legacy pid-only markers keep the previous behavior;
  unreadable tokens fail closed
- regression: live pid + wrong start token is not the same instance
  (fails when the token check is neutered)

packages/coding-agent/src/session/session-context.ts
@@ -347,16 +347,18 @@ export function buildSessionContext(
 			if (!isCustomMessageContent(entry.content)) return;
 			const normalized = normalizeCustomMessagePayload(entry);
 			const attribution = entry.attribution === undefined ? undefined : normalized.attribution;
-			pushMessage(
-				createCustomMessage(
-					normalized.customType,
-					normalized.content,
-					normalized.display,
-					normalized.details,
-					entry.timestamp,
-					attribution,
-				),
+			const replayed = createCustomMessage(
+				normalized.customType,
+				normalized.content,
+				normalized.display,
+				normalized.details,
+				entry.timestamp,
+				attribution,
 			);
+			// Persisted rollback ownership rides beside `details`, never in it.
+			if (entry.userTurn === true) replayed.userTurn = true;
+			if (entry.promptPrelude === true) replayed.promptPrelude = true;
+			pushMessage(replayed);
 		} else if (entry.type === "branch_summary" && entry.summary) {
 			pushMessage(createBranchSummaryMessage(entry.summary, entry.fromId, entry.timestamp));
 		}

Daviey/oh-my-pi

packages/coding-agent/src/session/session-context.ts
@@ -396,7 +396,12 @@ export function buildSessionContext(
 				return;
 			}
 			const message = customMessageEntryMessage(entry);
-			if (message) pushMessage(message);
+			if (message) {
+				// Persisted rollback ownership rides beside `details`, never in it.
+				if (entry.userTurn === true) message.userTurn = true;
+				if (entry.promptPrelude === true) message.promptPrelude = true;
+				pushMessage(message);
+			}
 		} else if (entry.type === "branch_summary" && entry.summary) {
 			pushMessage(createBranchSummaryMessage(entry.summary, entry.fromId, entry.timestamp));
 		}